85de9c3a65a60a4f002fb7d8786bc30d1a058550
Sla1mer <slameryt4@gmail.com>
2023-09-14 18:05:47 +0200

    Добавьте файлы проекта.

---
+++ Lab2Sadik/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите R1: ");
+double R1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите R2: ");
+double R2 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите R3: ");
+double R3 = double.Parse(Console.ReadLine());
+
+double v1 = (4 / 3) * Math.PI * Math.Pow(R1, 3);
+double v2 = (4 / 3) * Math.PI * Math.Pow(R2, 3);
+double v3 = (4 / 3) * Math.PI * Math.Pow(R3, 3);
+
+double Z = (v1 + v2 + v3) / 3;
+
+Console.WriteLine($"Z = {Z:F2}");
